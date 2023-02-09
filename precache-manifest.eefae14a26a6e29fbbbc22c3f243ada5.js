0b01415e7356adc1978222fc076df0e56f530d23
Helixpranay <pranaykumar31399@gmail.com>
2023-02-09 20:02:35 +0530

    Updates

---
+++ precache-manifest.eefae14a26a6e29fbbbc22c3f243ada5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61e43eece94ea4a396a1",
+    "url": "/TextUtils-React/static/css/main.5bb9f0f1.chunk.css"
+  },
+  {
+    "revision": "61e43eece94ea4a396a1",
+    "url": "/TextUtils-React/static/js/main.b2f9fc47.chunk.js"
+  },
+  {
+    "revision": "a4f9459a308288b6214f",
+    "url": "/TextUtils-React/static/js/runtime~main.02905c84.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/TextUtils-React/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/TextUtils-React/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "6338b838f3e09dd422c16b1c0382afa9",
+    "url": "/TextUtils-React/index.html"
+  }
+];
